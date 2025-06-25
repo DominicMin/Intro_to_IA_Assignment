546ecda470a3acdefe83360ca6e25c7903d4baef
Wu sirui <DSC2409040@xmu.edu.my>
2025-06-25 08:17:11 +0800

    Add files via upload

---
+++ src/pages/CampusAssistant.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChatApp from '../components/ChatApp'
+
+const CampusAssistant = () => {
+  return (
+    <div className="p-4">
+      <ChatApp />
+    </div>
+  )
+}
+
+export default CampusAssistant
